47e5c96ac3e0bba17ad44e72b654bacc1caa8ccd
Ashish Jagadish <Ashish.Jagadish@protonmail.com>
2021-03-01 18:05:14 +0530

    add srv file containing the message structure or data types for request and response repectively

--- scripts/iot_sensor_data_subscriber.py
+++ scripts/iot_sensor_data_subscriber.py
@@ -2,6 +2,7 @@
 import rospy
 from ros_fundamentals.msg import IoTSensor
 
+
 def subscribe():
     rospy.init_node("iot_sensor_data_subscriber", anonymous=True)
     rospy.Subscriber(name="iot_sensor_data_topic", data_class=IoTSensor, callback=logger)
